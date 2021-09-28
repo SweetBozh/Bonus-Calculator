cc739d1e163966b3289c6ab8d015a620adb087a1
warunyupa <warunyupa.ler@student.mahidol.ac.th>
2021-09-28 14:49:49 +0700

    edit products

--- src/bonusCalculator.java
+++ src/bonusCalculator.java
@@ -1,19 +1,17 @@
 import java.util.*;
-<<<<<<< HEAD
-class Product implements Comparable<Product>{
-=======
 import java.io.*;
 
 class Employee{
     private String name;
-    private 
+    //private 
 }
-class Product{
->>>>>>> 1e5ad0a2cbc3be77b16b8de17f6737f8699d07f7
-    private String nameProduct;
-    private double price;
 
-    Product(String n,double pr){
+class Product implements Comparable<Product>{
+    private String nameProduct;
+    private int price;
+    private int bonus, totalSalesUnit,totalSalesBaht;
+    
+    Product(String n,int  pr){
         nameProduct = n;
         price = pr;
     }
@@ -21,10 +19,30 @@ class Product{
     public int Comparable(Product other){
         if(this.price > other.price )
             return -1;
-        else if()
+        else if(this.price == other.price)
+            return 0;
+        else
+            return 1;
+     }
+
+     public void setBonus(double pr){
+        if(pr>=0 && pr<10000)
+            bonus = price * (1/100);
+        else if(pr>=10000 && pr<30000)
+            bonus = price * (15/1000);
+        else if(pr>=30000 && pr<50000)
+            bonus = price * (2/100);
+        else 
+            bonus = price * (25/1000);      
     }
+     
+    public void printProduct(){
+        System.out.printf("%s\t price = %,6d\t (bonus = %,5d)\ttotal sales = %,4d units\t%,7d\n",nameProduct,price,bonus,totalSalesUnit,totalSalesBaht);
+    }   
+
 
 }
+
 public class bonusCalculator {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
