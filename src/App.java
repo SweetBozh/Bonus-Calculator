4046712ac76f13e19e589f005ca8d8e11abf691d
Podolph <nalin_hippo@hotmail.com>
2021-09-28 11:28:46 +0700

    Start

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        String a = input.nextLine();
+        System.out.println("Hello, World!");
+        System.out.println(a);
+        input.close();
+    }
+}
