aaaa2a20e21f9dd61edf446d285d76ff40f52fc8
warunyupa <warunyupa.ler@student.mahidol.ac.th>
2021-09-28 14:08:33 +0700

    add

--- src/bonusCalculator.java
+++ src/bonusCalculator.java
@@ -1,5 +1,7 @@
 import java.util.*;
-
+class Product{
+    
+}
 public class bonusCalculator {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
@@ -7,7 +9,6 @@ public class bonusCalculator {
         System.out.println("Hello, World!");
         System.out.println(a);
         input.close();
-        System.out.println("YOO");
-        System.out.println("See");
+        
     }
 }
